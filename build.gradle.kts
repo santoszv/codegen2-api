b2bf6c1304985da3c7f0504fe27274bf3439f5e5
Santos Zatarain Vera <santoszv@Mac-mini-de-Santos.local>
2022-11-25 14:12:56 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+group = "mx.com.inftel.codegen"
+version = "2.0.0-SNAPSHOT"
+
+plugins {
+    java
+    `maven-publish`
+    signing
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl(file("$projectDir/build/repo"))
+//            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+//            credentials {
+//                username = ""
+//                password = ""
+//            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("codegen2Api") {
+            from(components["java"])
+            pom {
+                name.set("${project.group}:${project.name}")
+                description.set("Codegen2 API")
+                url.set("https://github.com/santoszv/codegen2-api")
+                inceptionYear.set("2022")
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("santoszv")
+                        name.set("Santos Zatarain Vera")
+                        email.set("santoszv@inftel.com.mx")
+                        url.set("https://www.inftel.com.mx")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/santoszv/codegen2-api")
+                    developerConnection.set("scm:git:https://github.com/santoszv/codegen2-api")
+                    url.set("https://github.com/santoszv/codegen2-api")
+                }
+            }
+            signing.sign(this)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+}
